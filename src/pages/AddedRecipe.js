507b4c38fa437e345804eb45e7b1116511703e17
A <alzemamia@gmail.com>
2024-07-03 20:33:13 +0300

    typo

--- src/pages/AddedRecipe.js
+++ src/pages/AddedRecipe.js
@@ -1,10 +1,9 @@
 import React from "react";
-import video from "../assets/gifvideo.gif";
 
 const AddedRecipePage = () => {
   return (
     <div className=" h-[100vh] overflow-hidden">
-      <img src={video} alt="pancake" className="object-contain" />
+      {/* <img src={video} alt="pancake" className="object-contain" /> */}
     </div>
   );
 };
